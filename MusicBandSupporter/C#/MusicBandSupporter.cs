3175571ced40069518849f2e57d5db0e9837b468
roderick-bishop11 <44269367+roderick-bishop11@users.noreply.github.com>
2019-08-08 11:24:26 -0400

    MusicBandSupporter-C#
    This is TestMusicBandSupporter, an introductory exercise to learn about test classes and working with objects from another class. This is the rewrite in C# for personal learning. In doing this I learned about class/namespace references in C# (no reference needed in Java) and I learned that default visibility does not allow access even in the same package/solution in C#.

---
+++ MusicBandSupporter/C#/MusicBandSupporter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicBandSupporter2
+{
+
+    public class MusicBandSupporter
+    {
+        private string name = "";
+        private int numBoxesSold;
+
+        public MusicBandSupporter(string name)
+        {
+            this.name = name;
+            numBoxesSold = 0;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public void updateSales(int num)
+        {
+            this.numBoxesSold += num;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0}:\t {1} boxes of bonbons sold.", name, numBoxesSold);
+        }
+    }
+}
---
+++ MusicBandSupporter/C#/TestMusicBandSupporter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicBandSupporter2;
+
+namespace TestMusicBandSupporter3
+{
+    class TestMusicBandSupporter
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter in the name of the first music band supporter");
+            String first = Console.ReadLine();
+            MusicBandSupporter student1 = new MusicBandSupporter(first);
+            Console.WriteLine("Enter in the name of the second music band supporter");
+            String second = Console.ReadLine();
+            MusicBandSupporter student2 = new MusicBandSupporter(second);
+
+            for (int i = 0; i < 3; i++)
+            {
+                int boxes;
+                Console.WriteLine("Enter in the number of boxes sold by " + student1.getName() + " during week # " + (i + 1));
+                boxes = Convert.ToInt32(Console.ReadLine());
+                student1.updateSales(boxes);
+                Console.WriteLine("Enter in the number of boxes sold by " + student2.getName() + " during week # " + (i + 1));
+                boxes = Convert.ToInt32(Console.ReadLine());
+                student2.updateSales(boxes);
+            }
+            Console.WriteLine("Results of supporting the Music Band are: ");
+            Console.WriteLine(student1.ToString());
+            Console.WriteLine(student2.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
