f9549f16f804141ecc82f4f0393dc86b6b69a78f
roderick-bishop11 <rbbishop11@gmail.com>
2020-10-10 01:28:00 -0400

    Simple program that chooses the GCD of two numbers

---
+++ JavaOnly/GCD/GCD.java
@@ -0,0 +1,35 @@
+/*
+ * Author: Roderick Bishop
+ * 
+ * Created: 1/20/2018
+ * 
+ * Given two integers from the user, the program recursively finds the greatest common divisor. 
+ */
+import java.util.Scanner;
+
+public class GCD {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Input two integers to find out their Greatest Common Divisor");
+		
+		Scanner input = new Scanner(System.in);
+		
+		int m = input.nextInt();																	   //1st integer
+		int n = input.nextInt();																	   // 2nd integer
+
+		
+		System.out.println( "The greatest common divisor of " + m + " and " + n + " is " + GCD(m,n));  //Print statement prints out the GCD to the console using GCD method
+
+	}
+
+
+																									   // Method returns the GCD of two integers, using recursion. 
+	public static int GCD(int a, int b) {
+		if((a % b) == 0) 
+			return b;
+		else 
+			return GCD(b, a % b);
+	}
+
+}
