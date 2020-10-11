4210278d4e3b5b00b396648f9d769744f2cdf608
roderick-bishop11 <rbbishop11@gmail.com>
2020-10-11 14:43:07 -0400

    basic input validation. Will update with exceptions

--- JavaOnly/BitOperations/src/BitOperations.java
+++ JavaOnly/BitOperations/src/BitOperations.java
@@ -1,4 +1,9 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
 public class BitOperations {
     //if i use arrays, then all of my operations are linear.... if i use linkedLists, then all of my operations have better time
 //TODO: set up the main in a loop so that I can continually run different numbers
@@ -6,12 +11,13 @@ public class BitOperations {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the bit operations operator!\nFirst, enter in a binary number");
         String num = sc.nextLine();
+        validateNumber(num);
         System.out.println("Now which operation would you like to do to your number? Enter in \"c\" for circular shift, \"l\" for logical shift, and \"a\" for arithmetic shift");
         String decision = sc.next();
         System.out.println("Now enter in a direction that you want to perform the shift in. \"l\" for left and \"r\" for right");
         decision+=sc.next();
         BinList binaryList = createList(num);
-        selectOP(decision, binaryList);
+        selectOP(validateString(decision), binaryList);
     }
 
     public static void selectOP(String s, BinList list){
@@ -40,17 +46,46 @@ public class BitOperations {
                 System.out.println("You chose to logically shift left and your new value is: \t");
                 list.logiShift(Direction.LEFT);
                 break;
+            case "q":
+                System.out.println("Thanks for using the bit operations program!");
+                System.exit(0);
         }
     }
     //TODO: complete validation method, remove spaces and make sure only the letters in the set of permissible values are in there
-    //just some basic validation on the inputs
-    public void validateString(String dec){
+    //just some basic validation on the decision
+    public static String validateString(String dec){
+        Character[] chars = {'l','r','c','a','q'};
+        Set<Character> validChars = new HashSet<Character>(Arrays.asList(chars));
+        StringBuilder validated = new StringBuilder();
+        //we want to remove spaces,
+        for(char i: dec.toCharArray()){
+            if(i == ' '){
+                System.out.println("Space skipped");
+                continue; //skip the spaces
 
+            }
+
+            //add the exception here
+            if(!validChars.contains(i)){
+                System.out.println("You entered a bad number, try again"); //we want to throw the exception here
+                break;
+            }
+            else{
+                validated.append(i); //add it to the validated string
+            }
+        }
+        return validated.toString();
     }
 
     //TODO: validate that the number is a BINARY number, will use exceptions here
     public static void validateNumber(String num){
-
+        for(char i: num.toCharArray()){
+            //throw exception here
+            if(i != '0' && i != '1'){
+                System.out.println("You didn't enter a binary number. Try again");
+                break;
+            }
+        }
     }
 
     public static BinList createList(String s){
