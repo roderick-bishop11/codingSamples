22a6608d9531b55e71f4504507215aef1f1a7af6
Roderick Bishop <rbbishop11@gmail.com>
2021-07-17 11:13:27 -0400

    found something to update in this folder

--- PythonOnly/Grade.py
+++ PythonOnly/Grade.py
@@ -3,13 +3,13 @@
 # Program: Program inpts a student name and a student average. Program then tells the user the letter equivalent
 name = input("Enter student's name: ")
 avg = int(input("Enter student's grade average: "))
-    if(avg>=90):
-        print(name," your average is: A")
-    elif(avg<90 and avg>=80):
-        print(name, " your average is: B")
-    elif(avg<80 and avg>=70):
-        print(name, " your average is: C")
-    elif(avg<70 and avg>=60):
-        print(name, " your average is: D")
-    elif(avg<60 and avg>=00):
-        print(name, " your average is: F")
+if(avg>=90):
+        print(name.capitalize(),"your average is: A")
+elif(avg<90 and avg>=80):
+        print(name.capitalize(), "your average is: B")
+elif(avg<80 and avg>=70):
+        print(name.capitalize(), "your average is: C")
+elif(avg<70 and avg>=60):
+        print(name.capitalize(), "your average is: D")
+elif(avg<60 and avg>=00):
+        print(name.capitalize(), "your average is: F")
